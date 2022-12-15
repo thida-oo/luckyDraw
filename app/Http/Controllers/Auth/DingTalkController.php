3501bbb56522fd04b25633ab47f7169681306013
thida-oo <tdo.nirvasoft@gmail.com>
2022-12-15 16:44:13 +0800

    dingTalk login update

--- app/Http/Controllers/Auth/DingTalkController.php
+++ app/Http/Controllers/Auth/DingTalkController.php
@@ -16,34 +16,29 @@ use Overtrue\Socialite\SocialiteManager;
 class DingTalkController extends Controller
 {
     public function reDirectDingTalk(){
-      
-        // DingTalkConstant::$METHOD_GET 要与下面调用接口url要求的保持一致
-        // $c = new DingTalkClient(DingTalkConstant::$CALL_TYPE_OAPI, DingTalkConstant::$METHOD_GET , DingTalkConstant::$FORMAT_JSON);
-        // $req = new OapiUserGetRequest();
-        // $req->setUserid("userid1");
-        // $resp=$c->execute($req, $accessToken,"https://oapi.dingtalk.com/user/get");
-        // var_dump($resp); die;
-        //$socialite = new SocialiteManager()
-        $url = "https://oapi.dingtalk.com/connect/qrconnect?appid=dingzqdssz4xawqpd6li&response_type=code&scope=snsapi_login&state=STATE&redirect_uri='http:\'/\'/127.0.0.1:8000\'/auth\'/dingtalk\'/callback'";
-        var_dump($url);die;
+    
+		return Socialite::driver('dingtalk')->redirect();
     }
 
     public function dingTalkCallback(Request $request){
-   echo "c=dingcallback"; die;
-    //     $dingUser =Socialite::create('dingtalk')->userFromCode($request->query('code'));
-    //     //$dingUser = Socialite::driver('dingtalk')->user();
-    //     echo "<pre>"; var_dump($dingUser); echo "</pre>"; die;
-    //     $findUser = User::where('google_id', $dingUser->id)->first();
-    //     if($findUser){
-    //         Auth::login($findUser);
-    //         return redirect('/home');
-    //     }else {
-    //         $newUser = User::create([
-    //             'name' =>$dingUser->name,
-    //             'email'=>$dingUser->email,
-    //             'password'=>Hash::make('oppoGoogle'),
-    //             'google_id'=>$dingUser->id
-    //         ]);
-    //     }
+
+        $dingUser = Socialite::with('dingtalk')->user();
+
+        $findUser = User::where('google_id', $dingUser->id)->first();
+
+        if($findUser){ die;
+            Auth::login($findUser);
+            return redirect('/home');
+        }else {
+            $newUser = User::create([
+                'name' =>$dingUser->name,
+                'email'=>is_null( $dingUser->email)? $dingUser->unionid : $dingUser->email,
+                'password'=>Hash::make('oppoGoogle'),
+                'google_id'=>$dingUser->id
+            ]);
+			
+			Auth::login($newUser);
+            return redirect()->back();
+        }
     }
 }
--- app/Http/Controllers/Setup/EventSettingController.php
+++ app/Http/Controllers/Setup/EventSettingController.php
@@ -7,37 +7,17 @@ use App\Models\EventSetting;
 use App\Models\Present;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class EventSettingController extends Controller
 {
     public function index(){
-        $products = Product::all();
         $e_settings = EventSetting::all();
-        return view('setup/event-setting', ['e_settings'=>$e_settings,'products'=>$products]);
+        return view('setup/event-setting', ['e_settings'=>$e_settings]);
     }
 
     public function create(){ 
         $present_lists = Present::all();
         $products = Product::all();
         return view('setup/event-setting', ['present_lists'=>$present_lists, 'products'=>$products]);
-    }
-    public function store(Request $request)
-    {
-        // dd($request->all());
-        $products = $request->input('product');
-        $presents = $request->input('present_id');
-        $products = implode(',', $products);
-        $presents = implode(',', $presents);
-
-        $eventSetting = new EventSetting();
-        $eventSetting->name=$request->input('name');
-        $eventSetting->event_start_time=$request->input('start_time');
-        $eventSetting->event_end_time=$request->input('end_time');
-        $eventSetting->product_id=$products;
-        $eventSetting->present_id=$presents;
-        $eventSetting->created_by=Auth::user()->id;
-        $eventSetting->save();
-        return redirect()->route('event-setting-create');
     }
 }
