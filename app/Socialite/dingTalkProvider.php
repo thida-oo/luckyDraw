1d04f266e63777018f858618f0a7a2c94631de61
thida-oo <tdo.nirvasoft@gmail.com>
2022-12-28 17:24:10 +0800

    dingtalk socialite

--- app/Socialite/dingTalkProvider.php
+++ app/Socialite/dingTalkProvider.php
@@ -2,33 +2,29 @@
 
 namespace App\Socialite;
 
-use AlibabaCloud\SDK\Dingtalk\Vlink_1_0\Models\GetFollowerInfoResponseBody\result\user;
 use GuzzleHttp\RequestOptions;
 use Illuminate\Support\Arr;
-
 use Laravel\Socialite\Two\AbstractProvider;
 use Laravel\Socialite\Two\InvalidStateException;
 use Laravel\Socialite\Two\ProviderInterface;
-use Laravel\Socialite\Two\User as TwoUser;
-use PhpParser\Node\Expr\FuncCall;
-use SocialiteProviders\Manager\OAuth2\User as OAuth2User;
+use Laravel\Socialite\Two\User;
 
 class dingTalkProvider extends AbstractProvider implements ProviderInterface {
 
     protected string $authURL = "https://login.dingtalk.com/oauth2/auth";
     protected string $getUserInfobyCode = "https://oapi.dingtalk.com/sns/getuserinfo_bycode";
-    //protected string $getUserInfoURL = "https://oapi.dingtalk.com/topapi/v2/user/get";
+
     protected string $getUserInfoURL = 'https:/api.dingtalk.com/v1.0/contact/users/me';
 
-    //protected string $userAccessTokenURL = "https://oapi.dingtalk.com/v1.0/oauth2/userAccessToken";
-    //protected string $userAccessTokenURL = 'https://api.dingtalk.com/v1.0/oauth2/userAccessToken';
-    protected string $userAccessTokenURL = 'https://api.dingtalk.com/v1.0/oauth2/ssoAccessToken';
+    protected string $userAccessTokenURL = 'https://api.dingtalk.com/v1.0/oauth2/userAccessToken';
 
-    protected $scopes = ['openid'];
+    
     private $openId;
     protected $unionId;
 
     protected $withUnionId = false;
+
+    protected $scopes = ['openid'];
 
     public function withUnionId($value = true)
     {
@@ -40,11 +36,8 @@ class dingTalkProvider extends AbstractProvider implements ProviderInterface {
 
 
     public function getAuthUrl($state){
-        //echo "auth URl <br>";
-        //dd($this->buildAuthUrlFromBase($this->authURL, $state));
-    //https://login.dingtalk.com/oauth2/auth?client_id=dingzqdssz4xawqpd6li&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fauth%2Fdingtalk%2Fcallback&scope=openid&prompt=consent&response_type=code&state=Uh5UMPtW8PJcSSiX2p2QgmCjgsiLwayApHL7EPLM
-        return $this->buildAuthUrlFromBase($this->authURL, $state);
 
+        return $this->buildAuthUrlFromBase($this->authURL, $state);
     }
 
     public function getCodeFields($state=null){
@@ -91,10 +84,6 @@ class dingTalkProvider extends AbstractProvider implements ProviderInterface {
             'code'          =>     $code,
             'refreshToken'  =>   'refresh_token,'
         ];
-        // $fields = [
-        //     'corpid' => $this->DINGTALK_CORP_ID
-        //     'ssoSecret' => $this->ssoSecret,
-        // ];
         
         return array_merge(parent::getTokenFields($code), $fields);
     }
@@ -110,40 +99,20 @@ class dingTalkProvider extends AbstractProvider implements ProviderInterface {
     }
 
     /**
-     * Determine if the current request / session has a mismatching "state".
-     *
-     * @return bool
-     */
-    protected function hasInvalidState()
-    { 
-        if ($this->isStateless()) {
-            return false;
-        }
-
-        $state = $this->request->session()->pull('state');
-
-        return empty($state) || $this->request->input('state') !== $state;
-    }
-
-
-
-    /**
      * {@inheritdoc}
      */
     public function user(){
-    
-    //echo "<pre>"; var_dump($this->request->all()); echo "</pre>";
 
-        // if($this->user()){   dd("inside user");
-        //     return $this->user(); 
-        // }
+        if($this->user()){  
+            return $this->user(); 
+        }
         
-        if($this->hasInvalidState()){    dd("after user");
+        if($this->hasInvalidState()){  
             throw new InvalidStateException();
         }
 
         $code = $this->request->input('authCode');
-        //dd($code);
+        
         $response = $this->getAccessTokenResponse($code);
 
         $token = Arr::get($response, 'accessToken');
@@ -154,7 +123,6 @@ class dingTalkProvider extends AbstractProvider implements ProviderInterface {
                         ->setRefreshToken(Arr::get($response, 'refreshToken'))
                         ->setExpiresIn(Arr::get($response, 'expireIn'))
                         ->setApprovedScopes(Arr::get($response, 'scope', ''));
-                        // explode($this->scopeSeparator, Arr::get($response, 'scope', ''))
 
     }
 
@@ -163,27 +131,16 @@ class dingTalkProvider extends AbstractProvider implements ProviderInterface {
 
     public function getAccessTokenResponse($code)
     { 
-        //dd($this->getTokenFields($code));
-        echo "<pre> header <br>";var_dump($this->getTokenHeaders($code));  echo "</pre>";
-        echo "<br>";
-        echo "<pre>";var_dump($this->getTokenFields($code));  echo "</pre><br>"; 
-        // dd($this->getTokenUrl(), [
-        //     RequestOptions::HEADERS => $this->getTokenHeaders($code),
-        //     RequestOptions::FORM_PARAMS => $this->getTokenFields($code),
-        // ]);
         $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-            RequestOptions::HEADERS => $this->getTokenHeaders($code),
-            RequestOptions::FORM_PARAMS => $this->getTokenFields($code),
+            RequestOptions::JSON => $this->getTokenFields($code),
         ]);
-       
-        return json_decode($response->getBody(), true);
+
+        return $this->changeBody($response);
     }
 
     public function changeBody($response): array
-    {
+    { 
         $result = \json_decode((string) $response->getBody(), true);
-       
-       // \is_array($result) || throw new InvalidArgumentException('Decoded the given response payload failed.');
 
         return $result;
     }
@@ -207,13 +164,13 @@ class dingTalkProvider extends AbstractProvider implements ProviderInterface {
     }
 
     public function mapUserToObject(array $user){
-        dd($user);
-        return (new OAuth2User())->setRaw($user)->map([
+
+        return (new User())->setRaw($user)->map([
             'id'   => $this->openId, 
             'unionid' => $this->unionId, 
             'nickname' => $user['nick'] ?? null,
-            'name' => $user['name'] ?? null,
-            'email' => null, 
+            'name' => $user['nick'] ?? null,
+            'email' => $user['email'] ?? null, 
             'avatar' => $user['avatarUrl'] ?? null,
         ]);
     }
