7f8e712ff1abb52831ad2df258b09c01b0775e05
Chit Kaung San (Blake) <cks.northstar@gmail.com>
2023-02-06 13:22:00 +0630

    update stock import

--- app/Http/Controllers/Setup/StocksController.php
+++ app/Http/Controllers/Setup/StocksController.php
@@ -8,15 +8,17 @@ use App\Models\Stocks;
 use App\Imports\StockImport;
 use App\Imports\ProductImport;
 use Maatwebsite\Excel\Facades\Excel;
-
+use DB;
 class StocksController extends Controller
 {
     public function _stockImport(Request $request)
     {
+        $products = DB::table("products")->select("p_code","p_name")->pluck("p_code","p_name");
+        // dd($products);
         $import_file = $request->file('file');
         if ($import_file) {
             try {
-                Excel::import(new StockImport, $import_file);
+                Excel::import(new StockImport($products), $import_file);
                 return view('setup/import')->with('success', 'Data successfully imported');
             } catch (Exception $e) {
 
--- app/Imports/StockImport.php
+++ app/Imports/StockImport.php
@@ -12,28 +12,45 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 use RealRashid\SweetAlert\Facades\Alert;
 set_time_limit(99999);
-class StockImport implements ToModel, WithHeadingRow,WithBatchInserts,WithChunkReading
+class StockImport implements ToModel, WithHeadingRow
+// ,WithBatchInserts,WithChunkReading
 {
+    private $products;
+
+    public function __construct($products)
+    {
+        $this->products = $products; 
+    }
+
     public function model(array $row)
     {   
+
         DB::table('stock')->where('imei_sn',$row['ime'])->delete();
-        
         if( $row['shipping_warehouse'] != "HQ_MDY"){
             // Store code get from store table $row['orderdepotname'] 
             $store_code = DB::table('store')->where('store_name','like','%'.$row['orderdepotname'].'%')->get();
        
             // Distributor code get from distributor table $row['distributor']
             $distributor_code = DB::table('distributors')->where('distributor_name','like','%'.$row['purchasing_channel'].'%')->get();
-            
-             if($store_code->count() > 0 || $distributor_code->count() > 0){
+            if(!isset($distributor_code)){
+                if($store_code->count() > 0 || $distributor_code->count() > 0){
             Alert::info("IMEI'".$row['ime']."' has error.Please check store'".$row['orderdepotname']."' and distributor '".$row['purchasing_channel']." exist or not'.");
+            }
+
         }
 
+
             // Product id get from product table $row['productname']
-            $product_id = DB::table('products')->where('p_name','like','%'.$row['productname'].'%')->get();
-            if($product_id->count() > 0){
-                Alert::info("Prouct '".$row['productname']."does not exist.'");
-            }
+            // $match_product = in_array($row['productname'],$this->products->toArray());
+        if(!isset($this->products[$row['productname']])){
+            Alert::info("Prouct '".$row['productname']."does not exist.'")->persistent('Dismiss');
+                $p_code = null;
+                }else{
+                    $p_code = $this->products[$row['productname']];
+                }
+            
+
+
             // Warehouse id get from warehouse table
             $warehouse_id = DB::table('warehouses')->where('name','like','%'.$row['shipping_warehouse'].'%')->get();
            
@@ -48,20 +65,20 @@ class StockImport implements ToModel, WithHeadingRow,WithBatchInserts,WithChunkR
                 'order_by' =>1,
                 'shipment_order_no' =>$row['shipment_no'],
                 'delivery_order_no' =>$row['saleorderno'],
-                'product_id' =>count($product_id) == 0 ? 0 : $product_id[0]->id,
+                'product_id' => $p_code,
             ]);
         }
 
 
         
      }
-     public function batchSize(): int
-    {
-        return 1000;
-    }
+    //  public function batchSize(): int
+    // {
+    //     return 1000;
+    // }
     
-    public function chunkSize(): int
-    {
-        return 1000;
-    }
+    // public function chunkSize(): int
+    // {
+    //     return 1000;
+    // }
 }
